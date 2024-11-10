091f44c677ed5b5e773ab0cbaff24eedf8af9350
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-11-10 11:19:10 -0300

    edit

--- cara caneca/search.py
+++ cara caneca/search.py
@@ -1,28 +1,16 @@
 import os
 import csv
 
-# Caminho da pasta 'images'
-folder_path = 'images'
+def list_directory_structure(root_dir, output_file):
+  with open(output_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+      for dirname in sorted(dirnames):
+        writer.writerow([os.path.join(dirpath, dirname)])
+      for filename in sorted(filenames):
+        writer.writerow([os.path.join(dirpath, filename)])
 
-# Nome do arquivo CSV de saída
-output_csv = 'file_names.csv'
-
-# Lista para armazenar os nomes dos arquivos
-file_names = []
-
-# Percorre todos os arquivos na pasta 'images' e adiciona o caminho completo
-for root, dirs, files in os.walk(folder_path):
-  for file in files:
-    file_names.append(file)
-
-# Ordena os nomes dos arquivos em ordem alfabética
-file_names = sorted(file_names)
-
-# Escreve os nomes dos arquivos no arquivo CSV
-with open(output_csv, mode='w', newline='') as csv_file:
-  writer = csv.writer(csv_file)
-  writer.writerow(['File Name'])  # Cabeçalho do CSV
-  for name in file_names:
-    writer.writerow([name])
-
-print(f'Os nomes dos arquivos foram salvos em {output_csv}')
+if __name__ == "__main__":
+  root_directory = 'images'
+  output_file = 'file_names.csv'
+  list_directory_structure(root_directory, output_file)
