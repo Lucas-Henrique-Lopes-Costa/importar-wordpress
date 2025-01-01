35a71033479207697deaaf57d6a1646e0293e76f
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2025-01-01 19:56:03 -0300

    edit

--- vidadeslots/main.py
+++ vidadeslots/main.py
@@ -34,7 +34,7 @@ with open(output_csv, mode="w", newline="") as csvfile:
                     "nome-da-franquia": franchise_name,
                     "foto-da-franquia": image_url,
                     "link-google-maps": "#",
-                    "tipo": "jdb",
+                    "tipo": "hacksaw",
                 }
             )
 
